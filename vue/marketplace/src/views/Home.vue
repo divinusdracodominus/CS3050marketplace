fe4056e95657aa4b3479f585a2f72df0190488a6
Bob-Wang275 <61071716+Bob-Wang275@users.noreply.github.com>
2024-10-13 19:21:24 -0400

    Create Home.vue

---
+++ vue/marketplace/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <ProductListing />
+  </div>
+</template>
+
+<script>
+import ProductListing from '@/components/ProductListing.vue'
+
+export default {
+  components: {
+    ProductListing
+  }
+}
+</script>
