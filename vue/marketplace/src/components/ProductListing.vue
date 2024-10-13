c9f7d760ad7917e9bfa0da01f6aa032b4568cba7
Bob-Wang275 <61071716+Bob-Wang275@users.noreply.github.com>
2024-10-13 19:11:46 -0400

    Create ProductListing.vue

---
+++ vue/marketplace/src/components/ProductListing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Product List</h2>
+    <ul>
+      <!-- Here we can add a loop rendering of the product list -->
+      <li>Product 1 - $10</li>
+      <li>Product 2 - $20</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductListing'
+}
+</script>
