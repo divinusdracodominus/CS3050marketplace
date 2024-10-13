17eb7156da5b24e3c9b19c43f04d7ccacbedce4c
Bob-Wang275 <61071716+Bob-Wang275@users.noreply.github.com>
2024-10-13 19:13:41 -0400

    Create Profile.vue

---
+++ vue/marketplace/src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Profile</h1>
+    <p>Welcome to your profile page!</p >
+    <EditProfile />
+  </div>
+</template>
+
+<script>
+import EditProfile from '@/components/EditProfile.vue'
+
+export default {
+  components: {
+    EditProfile
+  }
+}
+</script>
