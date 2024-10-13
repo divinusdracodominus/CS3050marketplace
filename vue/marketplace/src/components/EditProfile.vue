006612bbdf9cc3f7f4e0ebb0de862485efc1771e
Bob-Wang275 <61071716+Bob-Wang275@users.noreply.github.com>
2024-10-13 19:09:54 -0400

    Create EditProfile.vue

---
+++ vue/marketplace/src/components/EditProfile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Edit Profile</h2>
+    <!-- Here we can add a form to edit user information -->
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name">
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email">
+      <button type="submit">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditProfile',
+  data() {
+    return {
+      name: '',
+      email: ''
+    }
+  }
+}
+</script>
