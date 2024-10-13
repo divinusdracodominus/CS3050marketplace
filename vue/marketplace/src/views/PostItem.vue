f92c224f787162a838e10935903db2c4c4328298
Bob-Wang275 <61071716+Bob-Wang275@users.noreply.github.com>
2024-10-13 19:12:55 -0400

    Create PostItem.vue

---
+++ vue/marketplace/src/views/PostItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Post Item</h1>
+    <PostItem />
+  </div>
+</template>
+
+<script>
+import PostItem from '@/components/PostItem.vue'
+
+export default {
+  components: {
+    PostItem
+  }
+}
+</script>
